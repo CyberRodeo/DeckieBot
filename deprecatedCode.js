c85a422a0e78b210319bc8e0b72d30d4715d131e
Himanshu <72098121+CyberRodeo@users.noreply.github.com>
2023-05-13 14:16:32 +0530

    v3

---
+++ deprecatedCode.js
@@ -0,0 +1,18 @@
+
+//  Code used to read the files under the command folders. not used cause the commands are now in subfolders!
+
+//  client.commands = new Collection()
+
+//  const commandsPath = path.join(__dirname, 'commands');
+//  const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+//  for(const file of commandsFiles){
+//      const filePath = path.join(commandsFiles, file);
+//      const command = require(filePath)
+
+//      if('data' in command && 'execute' in  command){
+//          client.commands.set(command.data.name, command)
+//      } 
+//      else{
+//          console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property`)
+//      }
--- main.js
+++ main.js
@@ -6,26 +6,6 @@ require("dotenv").config();
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
-
-// Code used to read the files under the command folders. not used cause the commands are now in subfolders!
-
-// client.commands = new Collection()
-
-// const commandsPath = path.join(__dirname, 'commands');
-// const commandsFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-// for(const file of commandsFiles){
-//     const filePath = path.join(commandsFiles, file);
-//     const command = require(filePath)
-
-//     if('data' in command && 'execute' in  command){
-//         client.commands.set(command.data.name, command)
-//     } 
-//     else{
-//         console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property`)
-//     }
-// }
-
 client.commands = new Collection();
 const folderPaths = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(folderPaths);
@@ -76,8 +56,8 @@ client.on(Events.InteractionCreate, async interaction => {
     }
 });
 
-// client.once(Event.ClientReady, c => {
-//     console.log(`ready to log in as $(c.user.tag)`);
-// });
+client.once(Event.ClientReady, c => {
+    console.log(`ready to log in as $(c.user.tag)`);
+});
 
 client.login(process.env.token, console.log("bot is online!"));
