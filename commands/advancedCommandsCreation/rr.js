3b733a3486d277a1869c561c76714473692d1f50
cr <72098121+CyberRodeo@users.noreply.github.com>
2024-02-20 19:24:27 +0530

    embeds init

--- commands/advancedCommandsCreation/rr.js
+++ commands/advancedCommandsCreation/rr.js
@@ -1,10 +1,31 @@
 const {SlashCommandBuilder} = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("createreactionroles")
         .setDescription("This command creates a reaction roles in the corresponding channel!"),
     async execute(interaction){
-        interaction.reply("checking!!!!");
+        const exampleEmbed = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .setTitle('Some title')
+            .setURL('https://discord.js.org/')
+            .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+            .setDescription('Some description here')
+            .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+            .addFields(
+                { name: 'Regular field title', value: 'Some value here' },
+                { name: '\u200B', value: '\u200B' },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+            )
+            .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+            .setImage('https://i.imgur.com/AfFp7pu.png')
+            .setTimestamp()
+            .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+            
+            const channel = interaction.client.channels.cache.get('1106876336204677161');
+
+            channel.send({ embeds: [exampleEmbed] });
     }
 };
